869e72dada51cb732072d161b1c407ca48c78bee
Zhang yi <151607727+zyitom@users.noreply.github.com>
2024-01-26 10:24:01 +0800

    Add files via upload

--- src/postprocess.cc
+++ src/postprocess.cc
@@ -24,7 +24,7 @@
 #include <set>
 #include <vector>
 //#define LABEL_NALE_TXT_PATH "../model/coco_80_labels_list.txt"
-#define LABEL_NALE_TXT_PATH "/home/orangepi/Desktop/rknn-cpp-Multithreading/model/armor.txt"
+#define LABEL_NALE_TXT_PATH "../model/armor.txt"
 static char *labels[OBJ_CLASS_NUM];
 
 const int anchor0[6] = {10, 13, 16, 30, 33, 23};
